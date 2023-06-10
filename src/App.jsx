c3f1f655dae59eaf6e758c318141405b76bfa097
yash jain <yashjain07.yj@gmail.com>
2023-06-10 14:44:44 +0530

    Generated The Instance Of Our Smart Contract Successfully.

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,15 @@ import Card from '../src/Components/Card';
 
 import SeatChart from '../src/Components/SeatChart';*/
 
+import ContractAbi from '../src/artifacts/contracts/TicketOrganiser.sol/TicketOrganiser.json';
+
 function App() {
 
   const [account , setAccount] = useState(null);
+
+  const [contract, setContract ] = useState(null);
+
+  const [provider , setProvider] = useState(null);
 
   const {ethereum} = window;
 
@@ -50,6 +56,36 @@ function App() {
     }
   }
 
+  const getContractInstance = async() => {
+
+    if(typeof ethereum != "undefined" && account != null){
+
+        const contractAddress = '0xa919cF1B688ccEb16552A5FeD4178137D7748433';
+
+        const ABI = ContractAbi.abi;
+
+        const provider = await ethers.providers.Web3Provider(ethereum);
+
+        const signer = await provider.getSigner();
+
+        const contract = await ethers.Contract(
+
+            contractAddress,
+            ABI,
+            signer
+      )
+
+      setContract(contract);
+
+      setProvider(provider);
+
+      console.log(contract , signer);
+
+
+    }
+
+  }
+
   useEffect(() => {
 
     window.ethereum.on("chainChanged" , (chainId) => {
@@ -77,6 +113,8 @@ function App() {
 
 
     getConnectedAccounts();
+
+    getContractInstance();
 
 
   } , [account])
